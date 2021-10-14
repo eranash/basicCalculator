3b9c25a29259912839a2e4b7e20b136a554760f0
eranash <45369755+eranash@users.noreply.github.com>
2021-10-14 10:39:54 +0300

    Add files via upload

---
+++ assets/scripts/app.js
@@ -0,0 +1,76 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+function userInputToNum() {
+  return parseInt(userInput.value);
+}
+
+function craeteAndWriteOutput(operator, resultBefore, calcNum) {
+  const calcDescription = `${resultBefore} ${operator} ${calcNum}`;
+  outputResult(currentResult, calcDescription);
+}
+
+function writeToLog(operationID, prevResult, operationNum, newResult) {
+  const logEntry = {
+    operation: operationID,
+    prevResult: prevResult,
+    Number: operationNum,
+    result: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+function calculateResult(calcType) {
+  if (
+    calcType !== "ADD" &&
+    calcType !== "SUBTRACT" &&
+    calcType !== "MULTIPLY" &&
+    calcType !== "DEVIDE" ||
+    !inputNum
+  ) {
+    return;
+  }
+
+  const inputNum = userInputToNum();
+  const initialResult = currentResult;
+  let mathOperator;
+  if (calcType === "ADD") {
+    currentResult += inputNum;
+    mathOperator = "+";
+  } else if (calcType === "SUBTRACT") {
+    currentResult -= inputNum;
+    mathOperator = "-";
+  } else if (calcType === "MULTIPLY") {
+    currentResult *= inputNum;
+    mathOperator = "*";
+  } else if (calcType === "DEVIDE") {
+    currentResult /= inputNum;
+    mathOperator = "/";
+  }
+
+  craeteAndWriteOutput(mathOperator, initialResult, inputNum);
+  writeToLog(calcType, initialResult, inputNum, currentResult);
+}
+
+function add() {
+  calculateResult("ADD");
+}
+
+function subtract() {
+  calculateResult("SUBTRACT");
+}
+
+function multiply() {
+  calculateResult("MULTIPLY");
+}
+
+function divide() {
+  calculateResult("DEVIDE");
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
